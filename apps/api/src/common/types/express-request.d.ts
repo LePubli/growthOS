25303fb2b773d0282333026d6ecc739789c1b53e
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-22 14:07:12 +0200

    Add files via upload

--- apps/api/src/common/types/express-request.d.ts
+++ apps/api/src/common/types/express-request.d.ts
@@ -1,1 +1,10 @@
+import 'express';
+import { UserPayload } from '../interfaces/user-payload.interface';
+import { TenantContext } from '../interfaces/tenant-context.interface';
 
+declare module 'express' {
+  interface Request {
+    user?: UserPayload;
+    tenant?: TenantContext;
+  }
+}
