a3bec7fe7b2ff134d2c1df3023a4923225c1e22f
Agence Le Publicitaire <121939583+LePubli@users.noreply.github.com>
2026-05-24 23:42:08 +0200

    Update plugins.module.ts

--- apps/api/src/core/plugins/plugins.module.ts
+++ apps/api/src/core/plugins/plugins.module.ts
@@ -3,17 +3,20 @@ import { PluginsController } from './plugins.controller';
 import { PluginsService } from './plugins.service';
 import { PluginRegistryService } from './plugin-registry.service';
 import { PluginLoaderService } from './plugin-loader.service';
+import { PluginEngineService } from './plugin-engine.service';
 
 @Module({
   controllers: [PluginsController],
   providers: [
     PluginsService,
+    PluginEngineService,
     PluginRegistryService,
     PluginLoaderService,
   ],
   exports: [
     PluginsService,
-    PluginRegistryService,  // ← Exporté pour être injecté partout dans l'app
+    PluginEngineService,
+    PluginRegistryService,
     PluginLoaderService,
   ],
 })
